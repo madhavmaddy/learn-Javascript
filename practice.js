144c5ec7b5a72f59140cd47054dbd8b1c63f9cc3
madhavmaddy <madhav@getchaine.com>
2021-05-17 21:11:05 +0530

    first js code

---
+++ practice.js
@@ -0,0 +1,36 @@
+//Comment the multiple lines use /* */
+
+/*
+console.log(`Hii phavya 
+how are you
+hey
+`)
+
+console.log('Hii phavya how are you hey')
+*/
+
+// variables to store the values
+// var, let, const
+
+// let variable
+
+/* let firstNameAndSurName = 'Hii this is madhav'
+
+console.log(firstNameAndSurName)
+
+firstNameAndSurName = 123
+
+console.log(firstNameAndSurName) */
+
+//const can be assign only once. you can't reassign it
+/* const person = 123123
+
+console.log(person) */
+
+// console staement with var keyword
+var person = 'hello'
+
+person = 1235
+
+console.log('this is the value', person)
+console.log(`This is the value ${person}`)
