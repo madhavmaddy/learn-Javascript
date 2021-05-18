5499607de78281683154217a38b9c531c8e3fba6
madhavmaddy <madhav@getchaine.com>
2021-05-18 21:35:04 +0530

    added operator, function, object, array

--- practice.js
+++ practice.js
@@ -28,9 +28,90 @@ console.log(firstNameAndSurName) */
 console.log(person) */
 
 // console staement with var keyword
-var person = 'hello'
 
-person = 1235
+// var person = 'hello'
 
-console.log('this is the value', person)
-console.log(`This is the value ${person}`)
+// person = 1235
+
+// console.log('this is the value', person)
+// console.log(`This is the value ${person}`)
+
+//primitive values
+// let result
+// let result = 'this is the text message' //string literal
+// result = 123 //number literal
+// result = 12.5 //number literal
+// result = true //boolean
+// result = undefined // undefined
+// result = null //object
+
+// let value = null
+
+// console.log(value)
+
+//function
+/* function addition(number1, number2) {
+    console.log(number1 + number2)
+}
+function subtract(number1, number2) {
+    console.log(number1 - number2)
+}
+function multiply(number1, number2) {
+    console.log(number1 * number2)
+}
+
+addition(1, 5)
+subtract(5, 4)
+multiply(6, 6)
+multiply(7, 6) */
+
+//array
+
+// const persons = [
+//     'Madhav',
+//     'phavya',
+//     'paras',
+//     1234,
+//     true,
+//     false,
+//     undefined,
+//     null,
+// ]
+// console.log(persons)
+//use square bracket to find a specific value from array
+//Ex: person[1]
+
+//objects
+
+const perDetails = {
+    firstName: 'Madhav',
+    lastName: 'Maddy',
+    age: 25,
+    city: 'Yanam',
+    state: 'Puducherry',
+    country: 'India',
+}
+
+const button = 'firstName'
+
+console.log(perDetails[button])
+
+//dot operator is to fetch the value from an objects
+//square bracket is used to fetch the after user enter (this is dynamic value will change according to the user need)
+//to call a object use objectName with dot operator to get the value
+
+//|| (or statement)
+
+const value11 = 0 || '' || 5
+undefined
+value11
+5
+const value12 = 0 || 10 || 5
+undefined
+value12
+10
+const value13 = 0 || 0 || 0
+undefined
+value13
+
+// and operator denotes as &&
